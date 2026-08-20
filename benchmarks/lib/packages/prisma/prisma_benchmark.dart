8095bc4641c709d60cc17c9d288439d7733ce22a
crazidev <crazibeatdeveloper@gmail.com>
2026-08-20 17:23:14 +0100

    fix(benchmarks): remove unmodifiable Platform.environment modification in Prisma benchmark

--- benchmarks/lib/packages/prisma/prisma_benchmark.dart
+++ benchmarks/lib/packages/prisma/prisma_benchmark.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:orm/orm.dart';
 import 'package:orm_benchmarks/prisma_client/client.dart';
+
 import 'package:orm_benchmarks/prisma_client/prisma.dart';
 import 'package:orm_benchmarks/utils/base_benchmark.dart';
 import 'package:orm_benchmarks/utils/step_profiler.dart';
@@ -18,18 +17,6 @@ class PrismaOrmBenchmark implements OrmBenchmark {
 
   @override
   Future<void> init() async {
-    final candidatePaths = [
-      'prisma/prisma-query-engine',
-      'benchmarks/prisma/prisma-query-engine',
-      '../benchmarks/prisma/prisma-query-engine',
-    ];
-    for (final p in candidatePaths) {
-      final f = File(p);
-      if (f.existsSync()) {
-        Platform.environment['PRISMA_QUERY_ENGINE_BINARY'] = f.absolute.path;
-        break;
-      }
-    }
     _prisma = PrismaClient();
     await _prisma.$connect();
   }
